c8a79d3e55adb082502b85f6223b6e955d06f1e7
Eva Michalcak <eva.michalcak@gmail.com>
2017-12-10 18:50:30 +0100

    footer info about initiative + colaborators logos

--- footer.php
+++ footer.php
@@ -29,7 +29,13 @@
 
 	</div><!-- .bottom-bar -->
 	<div class="project-info">
+		<p>
+				<span class="lang-ca">Cada any es produeix una gran quantitat d'exposicions i esdeveniments d'art a Barcelona que mereixen la pena. En aquest sentit, la iniciativa <em>Art seen in BCN'17</em>, més que establir rànquings, vol fer-te descobrir una imatge innovadora, fresca i única de l'escena d'art a Barcelona. Fem ús de les noves tecnologies per apropar l'art de forma lúdica i enfortir el teu vincle amb producció creativa local.</span>
+				<span class="lang-es">Cada año se produce una gran cantidad de exposiciones y eventos de arte en Barcelona que merecen la pena. En este sentido, la iniciativa <em>Art seen in BCN’17</em>, más que establecer rankings, quiere hacerte descubrir una imagen inovadora, fresca y única de la escena de arte en Barcelona. Hacemos uso de las nuevas tecnologías para acercarte el arte de forma lúdica y fortalecer tu vínculo con producción creativa local.</span>
+				<span class="lang-en">Every year there is a large number of exhibitions and art events in Barcelona that are worthwhile. In this sense, the initiative <em>Art seen in BCN'17</em>, rather than establishing rankings, wants to make you discover an innovative, fresh and unique image of the art scene in Barcelona. We make use of new technologies to approach art in a playful way and strengthen your link with local creative production.</span>
+			</p>
 		<div class="info-col initiators">
+			
 			<p>
 				<span class="lang-ca">Una iniciativa de:</span>
 				<span class="lang-es">Una iniciativa de:</span>
@@ -39,11 +45,21 @@
 		</div><!-- .info-col -->
 		<div class="info-col colaborators">
 			<p>
-				<span class="lang-ca">Col·laboradors:</span>
-				<span class="lang-es">Colaboradores:</span>
+				<span class="lang-ca">Col·laboren:</span>
+				<span class="lang-es">Colaboran:</span>
 				<span class="lang-en">Collaborating:</span>
 			</p>
-			<p><a href="http://www.artssspot.com" target="_blank" class="logo-link--s"><img src="<?php echo get_bloginfo('template_url') ?>/img/logo-placeholder.png" width="50" height="50" alt=""></a><span class="spacer"></span> <a href="http://www.artssspot.com" target="_blank" class="logo-link--s"><img src="<?php echo get_bloginfo('template_url') ?>/img/logo-placeholder.png" width="50" height="50" alt=""></a><span class="spacer"></span> <a href="http://www.artssspot.com" target="_blank" class="logo-link--s"><img src="<?php echo get_bloginfo('template_url') ?>/img/logo-placeholder.png" width="50" height="50" alt=""></a></p>
+			<p>
+				<a href="http://www.poblenouurbandistrict.com/" target="_blank" class="pud"><img src="<?php echo get_bloginfo('template_url') ?>/img/pud-logo.png" width="60" height="55" alt="Poblenou Urban District"></a>
+				<span class="spacer"></span>
+				<a href="http://www.galeriescatalunya.com/" target="_blank" class="ggac"><img src="<?php echo get_bloginfo('template_url') ?>/img/ggac-logo.png" width="80" height="31" alt="Gremi de galeries d'art de Catalunya"></a>
+				<span class="spacer"></span>
+				<a href="http://www.younggalleryweekend.com/" target="_blank" class="ygw"><img src="<?php echo get_bloginfo('template_url') ?>/img/ygw-logo.png" width="56" height="48" alt="Young Gallery Weekend"></a>
+				<span class="spacer"></span>
+				<a href="http://www.lhdistrictecultural.cat/" target="_blank" class="streetart"><img src="<?php echo get_bloginfo('template_url') ?>/img/dclh-logo.png" width="80" height="35" alt="Districte cultural l'Hospitalet"></a>
+				<span class="spacer"></span>
+				<a href="http://www.bcnstreetart.xyz/" target="_blank" class="streetart"><img src="<?php echo get_bloginfo('template_url') ?>/img/streetart-logo.png" width="45" height="45" alt="BCN Street Art"></a>
+			</p>
 		</div><!-- .info-col -->
 	</div><!-- .project-info -->
 </section><!-- .site-branding -->
